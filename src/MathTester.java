bdd8af31682bd94a68d9baf60a9902ba13b1ae3c
McKenna Cash <mckenna.cash.2020@student.lhschools.org>
2018-09-11 08:12:24 -0400

    Finished

---
+++ src/MathTester.java
@@ -0,0 +1,37 @@
+public class MathTester {
+
+    public static void main(String[ ] args){
+        String firstWord;
+        String secondWord;
+        int firstNumber,secondNumber, total;
+
+        firstWord = "dog";
+        secondWord = "house";
+        firstNumber = 5 ;
+        secondNumber = 10 ;
+        total = firstNumber+secondNumber;
+
+        System.out.println(total);
+        total= total + 10;
+        System.out.println(total);
+        /*BMI= Kg/M*M
+        M = inch*(0.0254meters/1inch)
+        inch = M*(1inch/0.0254meters)
+         */
+        /*System.out.println(firstWord+secondWord);
+        System.out.println(firstWord+(firstNumber+secondNumber));
+        System.out.println("firstNumber+secondNumber is "+(firstNumber+secondNumber));
+        System.out.println("firstNumber-secondNumber is "+(firstNumber+secondNumber));
+        System.out.println("secondNumber / firstNumber is "+(secondNumber / firstNumber));
+        System.out.println("firstNumber * secondNumber is "+(firstNumber*secondNumber));
+        System.out.println("The modulus of the secondNumber by the firstNumber" +
+                " is "+(secondNumber%firstNumber));*/
+
+
+
+
+
+
+
+    }
+}
